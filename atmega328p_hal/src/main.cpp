46d1533586fead46c1dcd093fb4a954dcaf6a3f4
arivenkkataram-asj <arivenkkataram.asj@gmail.com>
2026-02-23 23:23:20 +0530

    verified calculator program using PS2 keyboard

--- atmega328p_hal/src/main.cpp
+++ atmega328p_hal/src/main.cpp
@@ -3,158 +3,147 @@
 #include "hal/hal_timer.h"
 #include "hal/hal_interrupt.h"
 #include "hal/hal_uart.h"
+void calc_reset(void);
+void calculator_input_data_handle(char single_data);
+void get_input_expression(void);
+int64_t calculate_expression(void);
+void Serial_println_int64(int64_t result);
+#define MAX_Buffer 50
+int8_t buffer =0;
+char full_expression[50];
+bool expression_ready_for_calc = false;
 
-#define MAX_Expression_LEN 50
+int numbers_operator_index = 0;
 
-char expression[MAX_Expression_LEN + 1]; 
-char buffer = 0;
-char index = 0;
-bool expression_ready = false; 
-int64_t numbers[25];
-char operators[25];
-char numbers_index =0;
-char operators_index = 0;
-int64_t result = 0;
-bool equation_processed = false;
+int64_t numbers_seperation[25];
+char operators_seperation[25];
 
-void calculator_init(){
-  hal_uart_init(9600);
-  char sentence[] = "Calculator is Ready";
-  hal_Serial_println((char *)sentence);
-  buffer =0;
-  index =0;
-}
-
-void calculator_input_handle(char data){
-
-  if ((data >= '0' && data <= '9')|| data == '+' || data == '-' || data == '/' || data == '*'){
-    if (index <= 50){
-      expression[index] = data;
-      index++;
-      hal_uart_tx_byte(data);
+void calculator_input_data_handle(char single_data){
+  if(buffer<MAX_Buffer ){
+    if((single_data >= '0' && single_data <= '9' )|| single_data == '-' || single_data == '+' || single_data == '/' || single_data == '*'){
+      full_expression[buffer] = single_data;
+      buffer++;
+      hal_uart_tx_byte(single_data);
     }
-  }
-  if (data == '\n' || data == '='){
-    expression[index] = '\0';
-    expression_ready = true;
-  }
-
-  if (data == '\b'){
-    if(index > 0){
-      index--;
-      expression[index]=' ';
-      hal_uart_tx_byte(data);
-      hal_uart_tx_byte(' ');
-    }
-  }
-}
-
-void tokenize_expression(){
-  numbers_index = 0;
-  operators_index = 0;
-  char current_number =0;
-  for (int i=0; expression[i] != '\0';i++){
-    if(expression[i] <= '9' && expression[i] >= '0'){
-      current_number = current_number*10 + (expression[i] - '0');
-    }
-    if(expression[i] == '+' || expression[i] == '-' || expression[i] == '/' || expression[i] == '*'){
-      numbers[numbers_index] = current_number;
-      operators[operators_index] = expression[i];
-      current_number = 0;
-      operators_index++;
-      numbers_index++;
-    }
-  }
-  numbers[numbers_index+1]=current_number;
-}
-
-void resolve_add_sub(char first_no_index, char second_no_index, char operator_symbols){
-    equation_processed = true;
-    if(operator_symbols == '+'){
-      result += numbers[first_no_index] + numbers[second_no_index];
-    }if(operator_symbols == '-'){
-      result += numbers[first_no_index] - numbers[second_no_index];
-    }
-}
-
-void resolve_mul_div(char first_no_intex,char second_no_index, char operator_symbols){
-    if(!equation_processed){
-      result=1;
-    }
-    equation_processed = true;
-    if(operator_symbols == '*'){
-      result *= numbers[first_no_intex] * numbers[second_no_index];
-    }
-    if(operator_symbols == '/'){
-      result *= numbers[first_no_intex] / numbers[second_no_index];
-
-    }
-}
-
-int64_t calculator_evaluate(){
-  tokenize_expression();
-  char No_index = 0;
-  
-  4if((operators_index + 1) == (numbers_index )){
-    for(char i=0; i< operators_index; i++){
-      if(operators[i] == '+' || operators[i] == '-'){
-        resolve_add_sub(No_index, No_index+1, operators[i]);
-        No_index+=2;
-      }if(operators[i] == '*' || operators[i] == '/'){
-        resolve_mul_div(No_index,No_index+1,operators[i]);
-        No_index +=2;
+    else if(single_data == '\n' || single_data == '='){
+      expression_ready_for_calc = true;
+      full_expression[buffer]='\0';
+      hal_Serial_println((char *)"");
+      //hal_Serial_println(full_expression);
+    }else if(single_data == '\b'){
+      if(buffer>0){
+        buffer--;
+        full_expression[buffer] = '\0';
+        hal_uart_tx_byte(single_data);
+        hal_uart_tx_byte(' ');
+        hal_uart_tx_byte(single_data);
+      }else{
+        calc_reset();
       }
     }
-  }else{
-    hal_Serial_println("Syntax error");
-
-  }
-  return result;
-}
-
-void calculator_process_input(){
-  char receive_data;
-  if(hal_uart_rx_available()){    
-      receive_data = hal_uart_rx_byte();
-      calculator_input_handle(receive_data);
+  }else if(buffer>=MAX_Buffer){
+    hal_Serial_println((char *)"Buffer overflow.");
+    calc_reset();
   }
 }
 
+void calc_reset(){
+  buffer = 0;
+  expression_ready_for_calc = false;
+}
 
-void calculator_print_result(int64_t result){
-    char int_string[65];
-    char i;
-    for( i=0; result != 0;i++){
-      char temp = result%10;
-      int_string[i] = temp + 32;
+void get_input_expression(){
+    char receive_single_data;
+    if(hal_uart_rx_available()){
+      receive_single_data = hal_uart_rx_byte();
+      calculator_input_data_handle(receive_single_data);
     }
-    int_string[i] = '\0';
+} 
 
-    hal_Serial_println((char *)int_string);
+void tokenization_expression(){
+  int64_t current_number=0;  
+  numbers_operator_index = 0;
+  for(int index=0;full_expression[index]!= '\0';index++){
+    if(full_expression[index] >= '0' && full_expression[index] <= '9'){
+      current_number = (current_number*10) + (full_expression[index] - '0');
+    }else if(full_expression[index] == '+' || full_expression[index] == '-' || full_expression[index] == '/' || full_expression[index] == '*'){
+      numbers_seperation[numbers_operator_index] = current_number;
+      current_number = 0;
+      operators_seperation[numbers_operator_index] = full_expression[index];
+      numbers_operator_index++;
+    }
+  }
+  numbers_seperation[numbers_operator_index] = current_number;
+  
 }
 
-void calculator_reset(){
-  index = 0;
-  expression[0] = '\0';
-  expression_ready = false;
+int64_t calculate_expression(){
+  tokenization_expression();
+  int64_t result_calculation=numbers_seperation[0];
+  int operand_index_temp=0;
+  for(int index_numbers=1; index_numbers<(numbers_operator_index+1);index_numbers++){
+    if(operators_seperation[operand_index_temp]== '+'){
+      result_calculation = result_calculation+numbers_seperation[index_numbers];
+    }else if(operators_seperation[operand_index_temp] == '-'){
+      result_calculation = result_calculation - numbers_seperation[index_numbers];
+    }else if(operators_seperation[operand_index_temp] == '*'){
+      result_calculation = result_calculation * numbers_seperation[index_numbers];
+    }else if(operators_seperation[operand_index_temp] == '/'){
+      result_calculation = result_calculation / numbers_seperation[index_numbers];
+    }
+    operand_index_temp++;
+  }
+  return result_calculation;
+}
+
+void Serial_println_int64(int64_t results){
+  //hal_uart_tx_byte('\n');
+  char result_string[64];
+  int i = 0;
+  if(results > 0){
+    
+    for(int64_t num=results; num != 0 ; num/=10){
+      char temp = (num%10) + '0';
+      result_string[i++] = temp;
+    }
+  }if(results < 0){
+    hal_uart_tx_byte('-');
+    results = -results;
+    for(int64_t num=results; num != 0 ; num/=10){
+      char temp = (num%10) + '0';
+      result_string[i++] = temp;
+    }
+  }
+  for(int j=i-1;j >= 0;j--){
+    hal_uart_tx_byte(result_string[j]);
+  }
+  
 }
 
 int main(void){
   hal_systick_init();
   hal_enable_interrupt();
   hal_gpio_init(13,'O'); 
-  calculator_init();
+  hal_uart_init(9600);
   uint32_t LED_timer = 0;
   uint8_t LED_state = 0;
-  int64_t final_result = 0;
+  hal_Serial_println((char *)"Calculator is ready");
+  hal_Serial_print((char *)"calc>>>");
   while(1){
-
-    calculator_process_input();
-    if(expression_ready){
-      final_result = calculator_evaluate();
-      calculator_print_result(final_result);
-      calculator_reset();
+    if(hal_timeout_expired(LED_timer, 500)){
+      LED_timer = hal_millis();
+      LED_state ^= 1;
+      hal_gpio_write(13,LED_state);
     }
-
+    get_input_expression();
+    
+    if(expression_ready_for_calc){
+      int64_t result = calculate_expression();
+      Serial_println_int64(result);
+      calc_reset();
+      hal_uart_tx_byte('\n');
+      hal_Serial_print((char *)"calc>>>");
+    }
+    
   }
 }
